ee2f06ac189759085e30a0d9d61f359add7ba1c7
Jakub Konefał <konefaljakub@gmail.com>
2020-09-26 00:20:46 +0200

    - Refactor

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function scrollToScale(){
-    $('body, html').animate({
-        scrollLeft: 1600
-    }, 1000);
+init();
+
+function scrollToScale() {
+  $('body, html').animate(
+    {
+      scrollLeft: 1600,
+    },
+    1000
+  );
 }
 
 let planetsAxis = document.querySelectorAll('.planet');
@@ -10,193 +15,200 @@ planetsAxis = [...planetsAxis];
 let planetsNav = document.querySelectorAll('.nav-right__go-to');
 planetsNav = [...planetsNav];
 
-planetsNav.forEach(planet =>{
-    planet.addEventListener('click', goToPlanet);
-})
+planetsNav.forEach((planet, planetIndex) => {
+  planet.addEventListener('click', () => goToPlanet(planetIndex));
+});
 
-function goToPlanet(){
-    for(var i=0; i<10; i++){
-        if(this == planetsNav[i])
-        $('body, html').animate({
-            scrollLeft: $(planetsAxis[i]).offset().left - 800
-        }, 2500);
-    }
+const constants = {
+  sunFromLeft: 2040,
+  moonDiameter: 3474,
+  astronomicUnitDistanceKm: 150000000,
+  lightMinuteDistanceKm: 17987520,
+};
+
+function goToPlanet(planetIndex) {
+  $('body, html').animate(
+    {
+      scrollLeft: $(planetsAxis[planetIndex]).offset().left - 800,
+    },
+    2500
+  );
 }
 
-function addActive(elem){
-    elem.addClass('active');
+function addActive(elem) {
+  elem.addClass('active');
 }
 
-function removeActive(elem){
-    elem.removeClass('active');
+function removeActive(elem) {
+  elem.removeClass('active');
 }
 
-function toggleActive(elem){
-    elem.toggleClass('active');
+function toggleActive(elem) {
+  elem.toggleClass('active');
 }
 
-let menuRight = $('.nav-right');
+const menuRight = $('.nav-right');
 
-function hideMenuRight(){
-    let showMenuBtn = $('.menu-right__show');
-    let hideMenuBtn = $('.menu-right__hide');
-    toggleActive(showMenuBtn);
-    toggleActive(hideMenuBtn);
-    menuRight.addClass('hidden');
+function hideMenuRight() {
+  const showMenuBtn = $('.menu-right__show');
+  const hideMenuBtn = $('.menu-right__hide');
+  toggleActive(showMenuBtn);
+  toggleActive(hideMenuBtn);
+  menuRight.addClass('hidden');
 }
 
-function showMenuRight(){
-    let showMenuBtn = $('.menu-right__show');
-    let hideMenuBtn = $('.menu-right__hide');
-    toggleActive(showMenuBtn);
-    toggleActive(hideMenuBtn);
-    menuRight.removeClass('hidden');
+function showMenuRight() {
+  const showMenuBtn = $('.menu-right__show');
+  const hideMenuBtn = $('.menu-right__hide');
+  toggleActive(showMenuBtn);
+  toggleActive(hideMenuBtn);
+  menuRight.removeClass('hidden');
 }
 
-function togglePlanetInfo(){
-    let nextElem = this.nextElementSibling;
-    $(nextElem).slideToggle();
+function togglePlanetInfo() {
+  const nextElem = this.nextElementSibling;
+  $(nextElem).slideToggle();
 }
 
-function toggleUnitsBar(){
-    let unitsMenu = $('.units-choice');
-    unitsMenu.toggleClass('active');
+function toggleUnitsBar() {
+  const unitsMenu = $('.units-choice');
+  unitsMenu.toggleClass('active');
 }
 
 let chosenUnits = 'px';
 
-function setUnits(units){
-    chosenUnits = units;
+function setUnits(units) {
+  chosenUnits = units;
 }
 
 let unitsBar = document.querySelectorAll('.unit');
 unitsBar = [...unitsBar];
-unitsBar.forEach(unit =>{
-    $(unit).on('click', function(){
-        const units = unit.innerHTML;
-        setUnits(units);
-        toggleUnitsBar();
-        countDistance();
-    })
+unitsBar.forEach((unit) => {
+  $(unit).on('click', function () {
+    const units = unit.innerHTML;
+    setUnits(units);
+    toggleUnitsBar();
+    countDistance();
+  });
 });
 
-function countDistance(){
-    let scrollFromSun = $(window).scrollLeft() - 2040;
-    let distanceLabel = $('.distance-counter__distance');
-    switch(chosenUnits){
-        case 'km':
-            scrollFromSun = scrollFromSun*3474;
-            break;
-        case 'px':
-            scrollFromSun = scrollFromSun;
-            break;     
-        case 'au':
-            scrollFromSun = scrollFromSun*3474/150000000;
-            break;
-        case 'lm':
-            scrollFromSun = scrollFromSun*3474/17987520;
-            break;
+function countDistance() {
+  const {
+    astronomicUnitDistanceKm,
+    lightMinuteDistanceKm,
+    moonDiameter,
+    sunFromLeft,
+  } = constants;
 
-    }
-       distanceLabel.text(+scrollFromSun.toFixed(1) + chosenUnits);
+  let scrollFromSun = $(window).scrollLeft() - sunFromLeft;
+  let distanceLabel = $('.distance-counter__distance');
+  switch (chosenUnits) {
+    case 'km':
+      scrollFromSun = scrollFromSun * moonDiameter;
+      break;
+    case 'au':
+      scrollFromSun = (scrollFromSun * moonDiameter) / astronomicUnitDistanceKm;
+      break;
+    case 'lm':
+      scrollFromSun = (scrollFromSun * moonDiameter) / lightMinuteDistanceKm;
+      break;
+  }
+  distanceLabel.text(+scrollFromSun.toFixed(1) + chosenUnits);
 }
 
-function showDistance(){
-    const sunFromLeft = 2040;
-    let distanceCounter = $('.distance-counter');
-    if($(window).scrollLeft() >= sunFromLeft){
-       addActive(distanceCounter);
-    }
-    else{
-        removeActive(distanceCounter);
-    }
+function showDistance() {
+  const { sunFromLeft } = constants;
+  let distanceCounter = $('.distance-counter');
+  if ($(window).scrollLeft() >= sunFromLeft) {
+    addActive(distanceCounter);
+  } else {
+    removeActive(distanceCounter);
+  }
 }
 
-function putLightBeam(x,y){
-    $('.lightbeam').css('left', x + 'px');
-    $('.lightbeam').css('top', y + 'px');
-    $('.lightbeam').css('z-index', '1');
-    $('.lightbeam').addClass('lightspeed');
-    changeCursor();
-    toggleLightspeedMsg();
+function putLightBeam(x, y) {
+  $('.lightbeam').css('left', `${x}px`);
+  $('.lightbeam').css('top', `${y}px`);
+  $('.lightbeam').css('z-index', '1');
+  $('.lightbeam').addClass('lightspeed');
+  changeCursor();
+  toggleLightspeedMsg();
 }
 
-function removeLightBeam(){
-    $('.lightbeam').removeClass('lightspeed');
-    $('.lightbeam').css('z-index', '0');
+function removeLightBeam() {
+  $('.lightbeam').removeClass('lightspeed');
+  $('.lightbeam').css('z-index', '0');
 }
 
 let defaultCursor = true;
 let lightSpeedBtnClicked = false;
 
-function changeCursor(){
-    if(defaultCursor == true){
-    $('body').css('cursor', "url('img/cursor.png'), auto");
-    defaultCursor = false;
-    }
-    else{
-        $('body').css('cursor', 'default');
-        defaultCursor = true;
-    }
-    lightSpeedBtnClicked = true;
+function changeCursor() {
+  const cursorAttrValue = defaultCursor
+    ? "url('img/cursor.png'), auto"
+    : 'default';
+
+  $('body').css('cursor', cursorAttrValue);
+  defaultCursor = !defaultCursor;
+  lightSpeedBtnClicked = true;
 }
 
-function disableLightspeedBtn(){
-    $('.nav-right__lightspeed-btn').css('pointer-events', 'none');
+function disableLightspeedBtn() {
+  $('.nav-right__lightspeed-btn').css('pointer-events', 'none');
 }
 
-function enableLightspeedBtn(){
-    $('.nav-right__lightspeed-btn').css('pointer-events', 'all');
+function enableLightspeedBtn() {
+  $('.nav-right__lightspeed-btn').css('pointer-events', 'all');
 }
 
-function toggleLightspeedMsg(){
-    let lightspeedMsg = $('.lightspeed-msg');
-    toggleActive(lightspeedMsg);
+function toggleLightspeedMsg() {
+  let lightspeedMsg = $('.lightspeed-msg');
+  toggleActive(lightspeedMsg);
 }
 
-$(window).on('scroll', countDistance);
-$(window).on('scroll', showDistance);
-$('.distance-counter__distance').on('click', toggleUnitsBar);
-$('.welcome-header__button').on('click', scrollToScale);
-$('.menu-right__hide').on('click', hideMenuRight);
-$('.menu-right__show').on('click', showMenuRight);
-$('.planets_names').on('click', togglePlanetInfo);
-$('.nav-right__lightspeed-btn').on('click', changeCursor);
-$('.nav-right__lightspeed-btn').on('click', toggleLightspeedMsg);
-$('.nav-right__lightspeed-btn').on('click', disableLightspeedBtn);
-$('.wraper').on("click",function(event){
- if(lightSpeedBtnClicked == true){
+function init() {
+  $(window).on('scroll', countDistance);
+  $(window).on('scroll', showDistance);
+  $('.distance-counter__distance').on('click', toggleUnitsBar);
+  $('.welcome-header__button').on('click', scrollToScale);
+  $('.menu-right__hide').on('click', hideMenuRight);
+  $('.menu-right__show').on('click', showMenuRight);
+  $('.planets_names').on('click', togglePlanetInfo);
+  $('.nav-right__lightspeed-btn').on('click', changeCursor);
+  $('.nav-right__lightspeed-btn').on('click', toggleLightspeedMsg);
+  $('.nav-right__lightspeed-btn').on('click', disableLightspeedBtn);
+  $('.wraper').on('click', function (event) {
+    if (lightSpeedBtnClicked) {
       putLightBeam(event.pageX, event.pageY);
       lightSpeedBtnClicked = false;
- }
-});
-$('.lightbeam').on('click', removeLightBeam);
-$('.lightbeam').on('click', enableLightspeedBtn);
+    }
+  });
+  $('.lightbeam').on('click', removeLightBeam);
+  $('.lightbeam').on('click', enableLightspeedBtn);
+}
 
+// Horizontal scroll
 
-
-// Horizontal scroll 
-
-(function() {
- function scrollHorizontally(e) {
-e = window.event || e;
-var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-document.documentElement.scrollLeft -= (delta*29); // Multiplied by 40
-document.body.scrollLeft -= (delta*29); // Multiplied by 40
-// e.preventDefault();
- }
- if (window.addEventListener) {
-// IE9, Chrome, Safari, Opera
-window.addEventListener("mousewheel", scrollHorizontally, false);
-// Firefox
-window.addEventListener("DOMMouseScroll", scrollHorizontally, false);
+(function () {
+  function scrollHorizontally(e) {
+    e = window.event || e;
+    var delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
+    document.documentElement.scrollLeft -= delta * 29; // Multiplied by 40
+    document.body.scrollLeft -= delta * 29; // Multiplied by 40
+    // e.preventDefault();
+  }
+  if (window.addEventListener) {
+    // IE9, Chrome, Safari, Opera
+    window.addEventListener('mousewheel', scrollHorizontally, false);
+    // Firefox
+    window.addEventListener('DOMMouseScroll', scrollHorizontally, false);
   } else {
-// IE 6/7/8
-window.attachEvent("onmousewheel", scrollHorizontally);
- }
+    // IE 6/7/8
+    window.attachEvent('onmousewheel', scrollHorizontally);
+  }
 })();
 
-$(window).scroll(function(){
-var scroll = $(window).scrollLeft();
-// console.log(scroll);
+$(window).scroll(function () {
+  var scroll = $(window).scrollLeft();
+  // console.log(scroll);
 });
